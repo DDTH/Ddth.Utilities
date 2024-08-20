362e5503601ce283aa6d74ebddf0be7d4bb45df7
Thanh Nguyen <btnguyen2k@gmail.com>
2024-08-20 23:13:33 +1000

    Fix(CI) coverage report

--- Ddth.Utilities.Tests/ReflectionDIHelperTest.cs
+++ Ddth.Utilities.Tests/ReflectionDIHelperTest.cs
@@ -1,7 +1,7 @@
-namespace Ddth.Utilities.Tests;
 using Microsoft.Extensions.DependencyInjection;
-using System;
+using System.Reflection;
 
+namespace Ddth.Utilities.Tests;
 [TestClass]
 public class ReflectionDIHelperTest
 {
@@ -10,19 +10,96 @@ public class ReflectionDIHelperTest
     [TestInitialize]
     public void Setup()
     {
-        _serviceProvider = new ServiceCollection().BuildServiceProvider();
+        _serviceProvider = new ServiceCollection()
+            .AddSingleton<IDummyName>(new DummyName())
+            .AddSingleton("Dummy String")
+            .BuildServiceProvider();
     }
 
     [TestMethod]
     public void TestCreateInstance()
     {
-        var obj = ReflectionDIHelper.CreateInstance<DummyClass>(_serviceProvider, typeof(DummyClass));
+        var obj = ReflectionDIHelper.CreateInstance<DummyClass>(
+            _serviceProvider,
+            typeof(DummyClass)
+        );
         Assert.IsNotNull(obj);
         Assert.AreEqual("Default", obj.Name);
+    }
+
+    [TestMethod]
+    public void TestCreateInstanceWithConstructor()
+    {
+        var obj = ReflectionDIHelper.CreateInstance<DummyClassWithConstructor>(
+            _serviceProvider,
+            typeof(DummyClassWithConstructor)
+        );
+        Assert.IsNotNull(obj);
+        Assert.AreEqual("Dummy", obj.Name);
+    }
+
+    [TestMethod]
+    public void TestCreateInstanceWithSelectedConstructorTypeArr()
+    {
+        var obj = ReflectionDIHelper.CreateInstance<DummyClassWithMultipleConstructor>(
+            _serviceProvider,
+            typeof(DummyClassWithMultipleConstructor),
+            new Type[] { typeof(string) }
+        );
+        Assert.IsNotNull(obj);
+        Assert.AreEqual("constructor 2", obj.Name);
+    }
+
+    [TestMethod]
+    public void TestCreateInstanceWithSelectedConstructorParameterInfoArr()
+    {
+        var pinfo = typeof(DummyClassWithMultipleConstructor).GetConstructors()[1].GetParameters()[0];
+        var obj = ReflectionDIHelper.CreateInstance<DummyClassWithMultipleConstructor>(
+            _serviceProvider,
+            typeof(DummyClassWithMultipleConstructor),
+            new ParameterInfo[] { pinfo }
+        );
+        Assert.IsNotNull(obj);
+        Assert.AreEqual("constructor 2", obj.Name);
     }
 }
 
 public class DummyClass
 {
     public string Name { get; set; } = "Default";
+}
+
+public interface IDummyName
+{
+    string Name { get; }
+}
+
+public class DummyName : IDummyName
+{
+    public string Name => "Dummy";
+}
+
+public class DummyClassWithConstructor
+{
+    public DummyClassWithConstructor(IDummyName name)
+    {
+        Name = name.Name;
+    }
+
+    public string Name { get; set; }
+}
+
+public class DummyClassWithMultipleConstructor
+{
+    public DummyClassWithMultipleConstructor(IDummyName _)
+    {
+        Name = "constructor 1";
+    }
+
+    public DummyClassWithMultipleConstructor(string _)
+    {
+        Name = "constructor 2";
+    }
+
+    public string Name { get; set; }
 }
